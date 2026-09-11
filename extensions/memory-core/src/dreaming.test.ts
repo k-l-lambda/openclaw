clawdbot/clawdbot

feat(memory-core): add dreaming.humanReadable for machine-only dreaming
Closes #79916.

Deployments that treat memory as an automation artifact currently have to
choose between the machine dreaming pipeline and the Dream Diary: disabling
`dreaming` also loses phase reports, signals, and promotion, while leaving it
on always pays for narrative generation and writes prose into the workspace.

Add `plugins.entries.memory-core.config.dreaming.humanReadable`, defaulting to
true so existing installs are unchanged. When false, dreaming keeps every
machine artifact and still promotes into MEMORY.md, but makes no
human-readable write:

- the light and REM narrative subagent calls are skipped
- the deep narrative subagent call and its fallback diary entry are skipped
- the `## Deep Sleep` summary is not appended to DREAMS.md

The separate phase report under `memory/dreaming/<phase>/YYYY-MM-DD.md` is the
machine artifact, so it is still written; only the DREAMS.md write is gated.
Skipped narratives report `status: "skipped"`, so the sweep summary's degraded
and pending counters stay accurate rather than treating machine-only mode as
degradation.

The issue sketched a nested `humanReadable: { enabled: false }` and invited a
different shape. A flat boolean is used here because there is no second
sub-option to group, and it matches the existing `enabled` / `verboseLogging`
keys; it can be widened to an object later without breaking the flat form.

Tests cover config resolution and default propagation to all three phase
resolvers, the manifest schema accept/reject, the deep write split (report
kept, DREAMS.md never created), and a light-phase sweep asserting no subagent
run while the inline block and phase signals are still produced. One test
guards `resolveShortTermPromotionDreamingConfig`, which rebuilds its result
field by field and would otherwise silently drop the flag.

extensions/memory-core/src/dreaming.test.ts
@@ -418,6 +418,7 @@ describe("short-term dreaming config", () => {
       maxAgeDays: 30,
       maxPromotedSnippetTokens: constants.DEFAULT_DREAMING_MAX_PROMOTED_SNIPPET_TOKENS,
       maxPriorEntryLossFraction: constants.DEFAULT_DREAMING_MAX_PRIOR_ENTRY_LOSS_FRACTION,
+      humanReadable: true,
       verboseLogging: false,
       storage: {
         mode: "separate",
@@ -461,6 +462,7 @@ describe("short-term dreaming config", () => {
       maxAgeDays: 30,
       maxPromotedSnippetTokens: 333,
       maxPriorEntryLossFraction: constants.DEFAULT_DREAMING_MAX_PRIOR_ENTRY_LOSS_FRACTION,
+      humanReadable: true,
       verboseLogging: true,
       storage: {
         mode: "separate",
@@ -503,6 +505,7 @@ describe("short-term dreaming config", () => {
       maxAgeDays: 45,
       maxPromotedSnippetTokens: 222,
       maxPriorEntryLossFraction: constants.DEFAULT_DREAMING_MAX_PRIOR_ENTRY_LOSS_FRACTION,
+      humanReadable: true,
       verboseLogging: false,
       storage: {
         mode: "separate",
@@ -541,6 +544,7 @@ describe("short-term dreaming config", () => {
       maxAgeDays: 30,
       maxPromotedSnippetTokens: constants.DEFAULT_DREAMING_MAX_PROMOTED_SNIPPET_TOKENS,
       maxPriorEntryLossFraction: constants.DEFAULT_DREAMING_MAX_PRIOR_ENTRY_LOSS_FRACTION,
+      humanReadable: true,
       verboseLogging: false,
       storage: {
         mode: "separate",
@@ -583,6 +587,17 @@ describe("short-term dreaming config", () => {
 
     expect(enabled.verboseLogging).toBe(true);
     expect(disabled.verboseLogging).toBe(false);
+  });
+
+  it("carries humanReadable through the promotion config rebuild", () => {
+    // This resolver copies fields one by one, so a dropped flag would silently
+    // leave machine-only deployments with the Dream Diary still enabled.
+    expect(resolveShortTermPromotionDreamingConfig({ pluginConfig: {} }).humanReadable).toBe(true);
+    expect(
+      resolveShortTermPromotionDreamingConfig({
+        pluginConfig: { dreaming: { humanReadable: false } },
+      }).humanReadable,
+    ).toBe(false);
   });
 
   it("falls back to defaults when thresholds are negative", () => {

k-l-lambda/openclaw

extensions/memory-core/src/dreaming.test.ts
@@ -403,6 +403,17 @@ describe("short-term dreaming config", () => {
       resolveMemoryDeepDreamingConfig({ pluginConfig: { dreaming: { phases: { deep } } } }),
     ).toMatchObject(expected);
   });
+
+  it("carries humanReadable into the deep promotion config", () => {
+    // The deep resolver feeds the promotion sweep; a dropped flag would silently
+    // leave machine-only deployments with the Dream Diary still enabled.
+    expect(resolveMemoryDeepDreamingConfig({ pluginConfig: {} }).humanReadable).toBe(true);
+    expect(
+      resolveMemoryDeepDreamingConfig({
+        pluginConfig: { dreaming: { humanReadable: false } },
+      }).humanReadable,
+    ).toBe(false);
+  });
 });
 
 describe("dreaming service reconciliation", () => {